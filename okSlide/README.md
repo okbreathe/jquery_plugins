dbab03c9af4bbc84c0e9c08abf6090a81a3cafd0
Asher Van Brunt <asher.vanbrunt@gmail.com>
2010-05-04 22:39:31 -0700

    Use anchors rather than the name attribute

--- okSlide/README.md
+++ okSlide/README.md
@@ -3,62 +3,38 @@
 
 ## Explanation
 
-okSlide sets up a series of panels that can be navigated through by clicking on
+okSlide creates a series of panels that can be navigated through by clicking on
 available choices on each panel. Each choice leads to another panel until we run
 out of panels. It is possible to set it up both in a linear fashion 
 (where choices are not dependent previous choices ), and hierarchically.
 
 ## Usage
 
-Navigation relies on proper naming of the id attribute of the containing
-panel and the name attribute of the links contained in the panel. 
+Navigation is relatively simple. Each link has an anchor that is
+the id of the panel that it leads to.  `<a href='#foo'>a link</a>`
+will lead to the panel `<li id='foo'>...</li>`. Clicking on the link 'foo'
+will slide the panel '#foo' in view. See the example for extended usage.
 
-The hierarchy is defined by appending ids of children to the id of their parent.
+Markup must be a list. Each `li` is a 'panel'.
 
-That is if you have a panel named "one", a direct descendant of this
-panel would have an id of "one-two", and a child of "one-two", would have
-the id of "one-two-three".  When speaking about panels I will refer only
-to the last segment of their id, that is panel "one-two-three" will be
-referred to as 'Panel three'.
-
-This defines the relationship:
-
-    one -> two -> three
-
-If instead you wanted to make panel 'bar' and 'baz' both direct descendants of
-'foo', you would give the child panels the id, 'foo-bar' and 'foo-baz' respectively.
-This would define the relationship:
-
-					/ bar
-		foo -
-					\ baz
-
-To move from one panel to the next, links (by default with the class 
-"more") are used. Links must have a *name* attribute that is
-the last segment of the *id* of the panel that they point to. Example:
-
-    one -> two
-
-Panel one would have an id of 'one', panel 'two' would have an id 'one-two'.
-Therefore a link on panel 'one' that links to panel 'two', would have a name
-attribute of 'two'.
+    <ul>
+      <li>...whatever...</li>
+       ...
+       <li>...whatever...</li>
+     </ul>
 
 okSlide doesn't do anything other than move from one panel to the next. In
 order to give it a purpose, take advantage of the various callbacks.
 
-options       | default          | description
-------------- | -------------    | -------------
-duration      | 200              | Duration of the animation
-panelClass    | 'panel'          | Class of the element that will be moved
-vertical      | false            | If true, panels will move up/down rather than left/right
-nextTrigger   | 'a.more'         | Selector of the element that will trigger forward movement
-prevClass     | 'prev'           | Class of the li that will trigger backwards movement.  Note: will be dynamically inserted.
-selectTrigger | "a:not('.more')" | A terminal link (a link doesn't go anywhere). Triggers afterSelect event when clicked.
-afterSelect   | null             | Callback after a terminal link is selected, receives trigger as 'this' and the event
-afterNext     | null             | Callback after panels are moved forward, receivers trigger as 'this' and the panel we're moving to
-afterPrev     | null             | Callback after panels are moved backward, receivers trigger as 'this' and the panel we're moving to
-
-
-## Help
-    
-If you're experiencing issues try setting an explicit width on the panels in css.
+options       | default       | description
+------------- | ------------- | -------------
+triggerEvent  | "click"       | Trigger showing next panel
+panelSelector | "li"          | Selector of the element that will be moved
+duration      | 200           | Duration of the animation
+vertical      | false         | If true, panels will move up/down rather than left/right
+afterSelect   | null          | Callback after a terminal link is selected, receives trigger as 'this'
+afterNext     | null          | Callback after panels are moved forward, receivers trigger as 'this' and the panel we're moving to
+afterPrev     | null          | Callback after panels are moved backward, receivers trigger as 'this' and the panel we're moving to
+prevClass     | "prev"        | Class of the li that will trigger backwards movement.  Note: will be dynamically inserted.
+backtrackText | Function      | Text used by the backtracking link
+backtrack     | Function      | Function to use to append a backtracking link.  Set to false to only allow one-way traversal. 'this' will be set to the panel
--- okSlide/jquery.okSlide.js
+++ okSlide/jquery.okSlide.js
@@ -1,185 +1,133 @@
 /*
  * jquery.okSlide
  *
- * Copyright (c) 2009 Asher Van Brunt | http://www.okbreathe.com
+ * Copyright (c) 2010 Asher Van Brunt | http://www.okbreathe.com
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
- * Date: 08/13/2009
+ * Date: 05/04/2010
  *
- * @projectDescription Simple Hierarchal Menu Slider
+ * @projectDescription Simple Hierarchical Menu Slider
  * @author Asher Van Brunt
  * @mailto asher@okbreathe.com
- * @version 1.0
+ * @version 1.1
  *
  * @id jQuery.fn.okSlide
  * @param {Object} Hash of settings, none are required.
  * @return {jQuery} Returns the same jQuery object.
  *
  * @id jQuery.fn.nextUntil
-*
+ *
  * Notes:
- *  - The plugin has only been tested with jQuery v 1.3.1
- *
- *  Markup example for $("#wrapper").okSlide();
- *  
- *   <div id="wrapper">
- *    <ul>
- *      <li class='panel'>...whatever...</li>
- *      ...
- *      <li class='panel'>...whatever...</li>
- *    </ul>
- *  </div>
- *
+ *	- The plugin requires jQuery v 1.4.2.
  */
 
-/* Explanation
- *   Navigates through a series of panels, based on attribute names and ids.
- *   Given:
- *   Panel one with an id of 'one'
- *   Panel two with an id of 'one-two'
- *   Panel three with an id of 'one-three'
- *
- *   Children of panels are designated by appending names after the parent
- *   Panel two and three are thought of of children of panel one because of the naming scheme. 
- *
- *   To move down the hierarchy, links have a name attribute with a segment that points further
- *   down the hierarchy
- *   E.g. Panels One would have links with name attributes 'two' or 'three' which would move the
- *   shown panel to two or three if clicked
- *
- *
- * Tips
- *   If you're experience weird navigation issues try setting the explicit width on the 'panels'
- */
 (function($) {
 
   $.fn.nextUntil = function(expr) {
     var match = [];
     this.each(function(){
-        for( var i = this.nextSibling; i; i = i.nextSibling ) {
-          // Make sure that we're only dealing with elements
-          if ( i.nodeType != 1 ){ continue; }
-          // stop if we find a match
-          if ( $.filter( expr, [i] ).length ){ break; }
-          match.push( i );
-        }
-      });
-
+      for( var i = this.nextSibling; i; i = i.nextSibling ) {
+        if ( i.nodeType != 1 ){ continue; }
+        if ( $.filter( expr, [i] ).length ){ break; }
+        match.push( i );
+      }
+    });
     return this.pushStack( match, arguments );
   };
 
   $.fn.okSlide = function(opts) {
-    var animating = false;
-
     opts = $.extend({      
-      duration     : 200,              // Duration of the animation
-      panelClass   : 'panel',          // Class of the element that will be moved
-      vertical     : false,            // If true, panels will move up/down rather than left/right
-      nextTrigger  : 'a.more',         // selector of the element that will trigger forward movement
-      prevClass    : 'prev',           // class of the li that will trigger backwards movement.  Note: will be dynamically inserted.
-      selectTrigger: "a:not('.more')", // A terminal link (a link doesn't go anywhere). Triggers afterSelect event when clicked.
-      afterSelect  : null,             // Callback after a terminal link is selected, receives trigger as 'this' and the event
-      afterNext    : null,             // Callback after panels are moved forward, receivers trigger as 'this' and the panel we're moving to
-      afterPrev    : null              // Callback after panels are moved backward, receivers trigger as 'this' and the panel we're moving to
+      triggerEvent : "click", // Trigger showing next panel
+      panelSelector: "li",    // Selector of the element that will be moved
+      duration     : 200,     // Duration of the animation
+      vertical     : false,   // If true, panels will move up/down rather than left/right
+      afterSelect  : null,    // Callback after a terminal link is selected, receives trigger as 'this'
+      afterNext    : null,    // Callback after panels are moved forward, receivers trigger as 'this' and the panel we're moving to
+      afterPrev    : null,    // Callback after panels are moved backward, receivers trigger as 'this' and the panel we're moving to
+      prevClass    : "prev",  // Class of the li that will trigger backwards movement.  Note: will be dynamically inserted.
+      backtrackText: function(){ return 'back'; },               // Text used by the backtracking link
+      backtrack    : function(link){ return this.prepend(link);} // Function to use to append a backtracking link.  Set to false to only allow one-way traversal
+                                                                 // 'this' will be set to the panel
     }, opts);
 
-    // this  - the trigger
-    // dir   - 'prev' or 'next' (direction)
-    // prevPanel - panel that we're moving from
-    function animate(dir,$prevPanel){
-      if (animating) { return false; }
-      var $trigger  = $(this),
-          $panel    = getNextPanel.call(this,dir,$prevPanel),
-          $list     = $prevPanel.closest("ul"),
-          direction = dir == 'next' ? "-=" : "+=",
-          callback  = dir == 'next' ? 'afterNext' : 'afterPrev',
-          params    = opts.vertical ?  
-            { marginTop:  direction  + $prevPanel.outerHeight() + "px" } : 
-            { marginLeft: direction  + $prevPanel.outerWidth()  + "px" };
-            
-      // hide all the panels between the one we are moving to, and the current one
-      $prevPanel.nextUntil('#'+ $panel.attr('id')).hide(); 
-      $panel.show();
+    var animating = false;
 
-      animating = true;
+    function animate(dir){
+      var next, params, resizeOn, 
+          trigger  = this,
+          cur      = this.closest(opts.panelSelector),
+          callback = opts[dir == '+=' ?  'afterPrev' : 'afterNext'],
+          list     = cur.closest("ul");
 
-      $list.animate(
-        params, { 
-          queue:false, 
-          duration:opts.duration, 
-          complete:function(){ 
-            animating = false;
-            if (dir == 'prev'){ $($prevPanel.data('parentLink')).remove(); }
-            if (opts[callback]) { opts[callback].call($trigger[0], $panel); } 
-          } 
-      });          
-    }
+      if (this.attr('href').length > 1) {
+        animating = true;
+        next      = $(this.attr('href'));
+        resizeOn  = dir == "-=" ? cur : next;
 
-    function getNextPanel(dir,$currentPanel){
-      var $panel;
-      if (dir == 'next') {
-        $panel  = $('#'+$currentPanel.attr('id') + "-" + $(this).attr('name'));
-      } else {
-        $panel  = $('#'+$currentPanel.attr('id').replace(/\-(\w+)$/,''));
+        if (dir == '-=' && opts.backtrack) { 
+          addBackLink(this, cur, next); 
+        }
+
+        if (opts.vertical) {
+          list.closest(".ui-slide-wrapper").height(next.outerHeight()+"px");
+        } else {
+          list.closest(".ui-slide-wrapper").width(next.outerWidth()+"px");
+        }
+
+        params = opts.vertical ?  { marginTop:  dir + resizeOn.outerHeight() + "px" } : { marginLeft: dir + resizeOn.outerWidth()  + "px" };
+
+        // hide all the panels between the one we are moving to, and the current one
+        cur.nextUntil(next).hide(); 
+        next.show();
+
+        return list.animate(
+          params, { 
+            queue:false, 
+            duration:opts.duration, 
+            complete:function(){ animating = false; if (callback) { callback.call(trigger,next) } } 
+        });          
       }
-      return $panel;
-    }
-
-    // Append a link to the top of current panel to the parent 
-    // this       - trigger
-    // $prevPanel - panel we're moving form
-    // $panel     - panel we're moving to
-    function appendPrevLinks($prevPanel){
-      var $panel      = getNextPanel.call(this,'next',$prevPanel),
-          $list       = $('> ul', $panel),
-          $parentLink = $("<li><a href='#' class='" + opts.prevClass + "'>" + $(this).html() +"</a></li>");
-
-      $list.prepend($parentLink);
-
-      $panel.data('parentLink', $parentLink);
-
-      $parentLink.click(function(e){
-        e.preventDefault();
-        animate.call(this,'prev',$panel);
-      });
+      if (opts.afterSelect) { opts.afterSelect.call(trigger); }
       return false;
     }
 
+    // When moving vertically, we need to take into account the appended link...so maybe append it before getting the height
+    function addBackLink(trigger,cur,next){
+      if (next.data('prevLink')) { return false; }
+      opts.backtrack.call(next,"<a href='#"+cur.attr('id')+"' class='" + opts.prevClass + "'>"+opts.backtrackText.call(trigger,cur)+"</a>");
+      next.data('prevLink', true);
+    }
+
     return this.each(function(){
-      var $wrapper  = $(this),
-          $list     = $('> ul', $wrapper),
-          $panels   = $('> li', $list),
-          w         = $panels.outerWidth(true), 
-          h         = $panels.outerHeight(true);
+      var list  = $(this),
+          items = list.children(), 
+          ct    = $("<div class='ui-slide-wrapper'></div>"),
+          w     = items.outerWidth(true),
+          h     = items.outerHeight(true);
+      ct
+      .css("overflow","hidden")
+      .insertBefore(list)
+      .append(list);
 
-      // Set the wrapper to width of the visible panel
-      $wrapper.css("overflow","hidden");
-      if ( opts.vertical ) {
-        $wrapper.height(h); 
-      }
-      $wrapper.width(w);
-      
-      // Set the list to width of the entire length of the panels
-      $list
-        .css('width',$panels.length*w)
-        .data('totalPanels', $panels.length);
-
-      if (!opts.vertical) {
-        $panels.css('float','left');
+      if (opts.vertical) {
+        ct.height(items.height())
+        list.height(h);
+      } else {
+        ct.width(items.width())
+        list.width(w);
+        items.css('float','left');
       }
 
       // Hide everything but the current
-      $panels.not(":first").hide();
+      items.slice(1).hide();
 
-      // Setup event handlers
-      $(opts.nextTrigger, $list).click(function(e){
+      list.delegate("a",opts.triggerEvent, function(e){
         e.preventDefault();
+        var t = $(e.currentTarget);
         if (animating) { return false; }
-        var $panel = $(this).closest("."+opts.panelClass);
-        appendPrevLinks.call(this, $panel);
-        animate.call(this,'next',$panel);
+        animate.call(t,opts.backtrack && t.hasClass(opts.prevClass) ? "+=" : "-=");
       });
-      $(opts.selectTrigger, $list).click(function(e){ e.preventDefault(); if(opts.afterSelect){ opts.afterSelect.call(this,e); }  });
+
     });
   };
 
