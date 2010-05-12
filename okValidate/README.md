70f68e9fdde5bd805852993aa8c2a84d5c613366
Asher Van Brunt <asher.vanbrunt@gmail.com>
2010-05-11 22:06:18 -0700

    Allow more flexible validations

--- okValidate/README.md
+++ okValidate/README.md
@@ -1,22 +1,34 @@
 # okValidate
 
-*A lightweight, customizable form validation plugin for jQuery 1.3 that just works*
+*A lightweight, customizable form validation plugin for jQuery 1.3 and 1.4 that just works*
 
 ## Explanation
 
-A reasonably sized (12kb uncompressed, 6.8kb minified) form-validation plugin.
-Actually it would only be 7 kb uncompressed, and 3.8kb minified, but the
-email address and url validator regexps are *ginormous* (you could easily remove
-them if you want to use a lighter regexp).
+A reasonably sized (13kb uncompressed, 7.4kb minified) form-validation plugin.
+It would only be 7 kb uncompressed, and 4kb minified, if not for the email
+address and url validator regexps which are *ginormous*.
 
 ## Usage
 
-okValidate favors convention over configuration. To validate a particular field,
-add a class to the field name of one or more validators.
+okValidate favors convention over configuration. To validate a particular
+field, add the name of a validator to the class of an input. For example, if we
+wanted to use the `required` validator on a particular input we would add the
+class "required"
 
     <input class='email required' name='email' type='text' />
 
-This will validate the field with the 'required' and 'email' validators (built-in).
+This would validate the field with the 'required' and 'email' validators (built-in).
+
+All validator keys are converted to regexps when testing the field class.
+This allows us the opportunity to use the match data in validations. Take the
+built-in length validator, `"length\\[(\\d+),?(\\d+)?\\]"` (note that the RegExp is escaped).
+This will match a class of "length[#,#]" where '#' is any digit.
+Matches from the regexp are applied to the validator function as arguments. The
+length validator looks like this:
+
+    function(min,max) { var len = this.val().length; return len > min && len < max; }
+
+It receives the first and the second digit (if given) as `min` and `max`.
 
 To add additional validators you must add properties to or extend the
 `$.fn.okValidate.validators` object and/or the `$.fn.okValidate.messages`
@@ -27,23 +39,31 @@ messages **must** have the same key.
       my_validator: /^foo$/
     });
     $.extend($.fn.okValidate.messages, {
-      my_validator: "#{name} answer was not 'foo'"
+      my_validator: "#{field} answer was not 'foo'"
     });
 
-'Validations' and 'Validation Messages' live in separate objects 
-to allow redefining of of validations separately from the messages (validations are 
-mostly identical - messages can be more unique).
+'Validations' and 'Validation Messages' live in separate objects to allow
+redefining of of validations separately from the messages.
 
 Validators can be either a function or a regexp. If a function, it
 should return true if the input is valid, and false if it is not. Regexps should
 attempt to match a valid input.
 
-When writing messages, you can add replacement patterns. Anything inside 
-of the `#{...}` will be replaced with the corresponding attribute of the input.
-That is to say `#{name}` will be replaced with the name attribute from
-the input, and `#{id}` with the id. Additional transformation of this property
-will be performed by the 'replacementFormatter' method. The default is to
-replace underscores and dashes with spaces and to strip brackets.
+Validation messages can either be a string or a function that returns a string.
+If the message is a string, `#{field}` will be replaced with the results of the
+`fieldName` function. If the corresponding validator returns 1 or more matches,
+they can be substitued into the message using the format `#{n}` where
+'n' is the position of the match.  For example, the length message could be defined as:
+
+    "#{field} must be between #{1} and #{2} characters",
+
+If the validation message is a function, it will receive the fieldName as the first
+argument, and as matches as subsequent arguments. For example, the length
+message could be defined as:
+
+    function(name,min,max){
+      return name + " must be between "+min+" and "+max+" characters";
+    }
 
 ## Usage
 
@@ -51,9 +71,8 @@ replace underscores and dashes with spaces and to strip brackets.
 
 #### Note
 
-When validating checkbox and radio groups you only need to add validation
-class(es) to one checkbox or radio button in the group. It will still work if
-you add the classes to additional inputs however.
+When validating checkbox and radio groups you only _need_ to add validation
+class(es) to one input in the group. 
 
 ## Options
 
@@ -61,11 +80,10 @@ options             | default          | description
 -------------       | -------------    | -------------
 inlineErrors        | true             | If errors inline appear after inputs with errors or at the top of the form
 liveValidation      | true             | If validation occurs after the liveEvent is fired within the field or on submit
-liveEvent           | 'blur'           | Can be 'blur' or 'keyup'
-errorClass          | 'error'          | class added to inputs with errors and the error label appended to input fields
+liveEvent           | 'blur'           | Should be 'blur' or 'keyup'
+errorClass          | 'error'          | Class added to inputs with errors and the error label appended to input fields
 errorContainerClass | 'error-messages' | If not using inline errors, this is the class that the error list will have
 showErrorFunction   | null             | If you want a custom function for showing the errorlist
 hideErrorFunction   | null             | If you want a custom function for hiding the errorlist
 onSubmit            | null             | Callback on submit
-replacementFormatter| function()       | Alter the replacement pattern before insertion. 
-
+fieldName           | function(input)  | The result of this function will be substituted into the error message for #{field}
--- okValidate/jquery.okValidate.js
+++ okValidate/jquery.okValidate.js
@@ -1,22 +1,22 @@
 /*
  * jquery.okValidate 
  *
- * Copyright (c) 2009 Asher Van Brunt | http://www.okbreathe.com
+ * Copyright (c) 2010 Asher Van Brunt | http://www.okbreathe.com
  * Dual licensed under MIT and GPL.
  * http://www.opensource.org/licenses/mit-license.php
- * Date: 01/10/2010
+ * Date: 05/11/10
  *
  * @projectDescription Simple Validation for forms
  * @author Asher Van Brunt
  * @mailto asher@okbreathe.com
- * @version 0.5
+ * @version 0.6
  *
  * @id jQuery.fn.okValidate
  * @param {Object} Hash of settings, none are required.
  * @return {jQuery} Returns the same jQuery object
  *
  * Notes:
- *	- The plugin has only been tested with jQuery v 1.3.1, but should work with 1.4 as well
+ *	- The plugin has only been tested with jQuery v 1.3.1, v 1.4.2 
  */
 
 ;(function($){
@@ -32,14 +32,14 @@
     opts = $.extend({
       inlineErrors: true,                           // If true errors will be appended after the offending inputs, if false will appear at the top of the form
       liveValidation: true,                         // If true errors will appear after the field loses focus, if false only after submit is clicked
-      liveEvent: 'blur',                            // Can be 'blur' or 'keyup'
-      errorClass: "error",                          // The class that will be added to inputs with errors (only for inlineErrors)
-      errorContainerClass: "error-messages",        // If not using inline errors, errors will be wrapped in a label of this class
-      showErrorFunction: null,                      // If you want a custom function fow showing the errorlist
-      hideErrorFunction: null,                      // If you want a custom function fow hiding the errorlist
-      onSubmit: null,                               // Callback on submit
-      replacementFormatter: function(replacement) { // Alter the replacement pattern before insertion. Default: replaces underscores/dashes with spaces, strips brackets
-        return replacement.replace(/[_-]+/g,' ').replace(/\[\]/, '');
+      liveEvent: 'blur',                            // Should be 'blur' or 'keyup'
+      errorClass: "error",                          // The class that will be added to inputs with errors (note: only for inlineErrors)
+      errorContainerClass: "error-messages",        // When not using inline errors, errors will be wrapped in a label with this class
+      showErrorFunction: null,                      // Custom function for showing the errorlist
+      hideErrorFunction: null,                      // Custom function for hiding the errorlist
+      onSubmit: null,                               // Alternate function to call instead of default action
+      fieldName: function(input){                   // The result of this function will be substituted into the error message for #{field}
+        return input.attr('name').replace(/[\[\]_-]+/g,' ');
       }
     }, opts);
 
@@ -49,15 +49,20 @@
 
     // We don't necessarily want to append directly to the input because they
     // are commonly wrapped in labels
-    function getErrorElement($input){
-      return  $input.parent()[0].tagName == "LABEL" ? $input.parent() : $input;
+    function getErrorElement(input){
+      return  input.parent()[0].tagName == "LABEL" ? input.parent() : input;
     }
 
-    // Given the replacement pattern "#{attribute}", will replace the contents
-    // with the attribute as applied to the input
-    function generateMessage($input, msg) {
-      var replacement = msg.match(/#\{([^\{}]+)\}/);
-      return replacement !== null ?  msg.replace(/#\{[^\{]+\}+/, opts.replacementFormatter($input.attr(replacement[1]))) : msg;
+    function generateMessage(msg,matches) {
+      if (typeof(msg) == "function") {
+        msg = msg.apply(this,[opts.fieldName(this)].concat(matches));
+      } else {
+        msg = msg.replace("#{field}", opts.fieldName(this))
+        for (var i=0;i<matches.length;i++) {
+          msg = msg.replace("#{"+(i+1)+"}", matches[i]);
+        }
+      }
+      return msg;
     }
 
     // Constructs the error markup
@@ -100,147 +105,166 @@
     }
 
     // Errors appended to individual fields
-    function appendErrorsTo($input) {
-      var $label = $("<label/>"),
-          msg    = $input.data('messages').join(", ");
+    function appendErrorsTo(input) {
+      var label = $("<label/>"),
+          msg    = input.data('messages').join(", ");
       if (opts.showErrorFunction) {
         opts.showErrorFunction.call(this); 
       } else {
-        if ($input.data('label')) {
-          $input.data('label').html(msg);
+        if (input.data('label')) {
+          input.data('label').html(msg);
         } else {
-          $input.addClass(opts.errorClass);
-          $label
+          input.addClass(opts.errorClass);
+          label
             .addClass(opts.errorClass)
             .text(msg)
-            .insertAfter(getErrorElement($input));
-          $input.data('label', $label);
+            .insertAfter(getErrorElement(input));
+          input.data('label', label);
         }
       }
     }
 
-    function removeErrorsFrom($input) {
+    function removeErrorsFrom(input) {
       if (opts.hideErrorFunction) {
         opts.hideErrorFunction.call(this); 
       } else {
-        if ($input.data('label')) {
-          $input.data('label').remove();
-          $input
+        if (input.data('label')) {
+          input.data('label').remove();
+          input
             .data('label',null)
             .removeClass(opts.errorClass);
         }
       }
     }
 
-    function maybeDisplayErrors($form,event) {
+    function maybeDisplayErrors(form,event) {
       if (opts.inlineErrors && this && this.data('messages').length <= 0) {
         removeErrorsFrom(this);
         return true;
-      } else if (isEmpty( $form.data('errors') )) {
-        removeErrorList.call($form);
+      } else if (isEmpty( form.data('errors') )) {
+        removeErrorList.call(form);
         return true;
       } 
 
       opts.inlineErrors ?  
-        $.each( $form.data('errors'),function(k,v){ appendErrorsTo($(v)); }) :
-        displayErrorList.call($form);
+        $.each( form.data('errors'),function(k,v){ appendErrorsTo($(v)); }) :
+        displayErrorList.call(form);
 
       if (event) {
+        event.stopPropagation();
         event.preventDefault();
       }
       return false;
     }
 
-    /* This is the where the meat of the processing occurs. If the input has a
+    /*
+     * This is the where the meat of the processing occurs. If the input has a
      * class matching an existing validator name we will attempt to validate
      * against it.  If it isn't valid add a message to the messages array.
      * Finally, if the input contains any error messages append it to form's
      * error field list otherwise remove it from the list
-    */
-    function validateInput($form) {
-      var $input           = this,
-          assign_or_get_id = function (i) { if(!i.attr('id')){i.attr('id', "input_" + parseInt(new Date().getTime()+Math.random(), 10));}},
-          validator;
-      $input.data('messages', []);
-      $.each($input[0].className.split(" "), function(){
-        validator = validators[this];
-        if (validator && !(typeof(validator) == 'function' ? validator.call($input) : validator.test($input.val()))) {
-          if (this == 'required' || ($input.val() != "")) { // Prevent empty inputs from triggering validation errors
-            assign_or_get_id($input);
-            $input.data('messages').push(generateMessage($input, messages[this]));
-            $form.data('errors')[$input.attr('id')] = $input;
+     */
+    function validateInput(input,form) {
+      var validatorName, validator, matches,
+          compiled         = compileValidators(),
+          i                = compiled.length,
+          assign_or_get_id = function(i) { if(!i.attr('id')){i.attr('id', "input_" + parseInt(new Date().getTime()+Math.random(), 10));} };
+
+      input.data('messages', []);
+
+      while(i--) {
+        matches = input[0].className.match(compiled[i][0]);
+        if (matches) {
+          validatorName = compiled[i][2];
+          validator     = compiled[i][1];
+          if (!(typeof(validator) == 'function' ? validator.apply(input,matches.slice(1)) : validator.test(input.val()))) {
+            if (validatorName == 'required' || (input.val() != "")) { // Prevent empty inputs from triggering validation errors
+              assign_or_get_id(input);
+              input.data('messages').push(generateMessage.call(input, messages[validatorName], matches.slice(1)));
+              form.data('errors')[input.attr('id')] = input;
+            }
           }
         }
-      });
+      }
 
-      if ( $input.data('messages').length <= 0 ) {
-        delete $form.data('errors')[$input.attr('id')];
+      if ( input.data('messages').length <= 0 ) {
+        delete form.data('errors')[input.attr('id')];
       }
     }
 
+    // Returns [RegExp,Validator,Validator Name]
+    function compileValidators(){
+      var arr=[]; 
+      for(var key in $.fn.okValidate.validators) { 
+        arr.push([new RegExp(key), $.fn.okValidate.validators[key], key]); 
+      } 
+      return arr; 
+    }
+
     return this.each(function(){
-      var $form   = $(this),
-          $inputs = $("input[type='checkbox'], input[type='password'], input[type='radio'].required, input[type='text'], select, textarea", $form),
+      var form   = $(this),
+          inputs = $("input[type='checkbox'], input[type='password'], input[type='radio'].required, input[type='text'], select, textarea", form),
           onEvent = function(e){
-            // Only test the last item of a check of radiobox group
-            var $input = /(checkbox|radio)/.test(this.type) ?  $("input[type='"+RegExp.$1+"'][name='"+this.name+"']:last", $form) : $(this);
-            validateInput.call($input, $form);
-            maybeDisplayErrors.call($input, $form, e);
+            // Only test the last item of a checkbox or radio group
+            var input = /(checkbox|radio)/.test(this.type) ?  $("input[type='"+RegExp.$1+"'][name='"+this.name+"']:last", form) : $(this);
+            validateInput(input, form);
+            maybeDisplayErrors.call(input, form, e);
           }
 
-      $form.data('errors', {});
+      form.data('errors', {});
 
       if (opts.liveValidation) {
-        $inputs[opts.liveEvent](function(){ onEvent.call(this); });
+        inputs[opts.liveEvent](function(){ onEvent.call(this); });
       } 
 
-      $form.submit(function(e){
-        $inputs.each(function(){ onEvent.call(this,e); });
+      form.submit(function(e){
+        inputs.each(function(){ onEvent.call(this,e); });
         if (opts.onSubmit) {
-          opts.onSubmit.call($form,e); 
+          opts.onSubmit.call(form,e); 
         }
       });
     });
   };
 
-  /* Extend these objects to add additional validators and messages. Validators
-   * should have the same key as the message that they apply to. Validators can
+  /*
+   * Extend these objects to add additional validators and messages Validators
+   * should have the same key as the message that they apply to Validators can
    * be RegExp or a function - if a function returns true the input is
    * considered valid.
-   *
-   * If you don't want an validator to validate, just extend the validator 
+   * 
+   * If you don't want an validator to validate, just extend the validators 
    * object with a function that returns true.
-   *
-   * Note: Inside the validators 'this' is set to the input currently being validated against
-   * Email and URL Regexs from: http://bassistance.de/jquery-plugins/jquery-plugin-validation/
-  */ 
+   * Inside the validators 'this' is set to the input currently being validated against
+   */
   $.fn.okValidate.validators = {
     email:/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
+    usd:/^\$?(\d{1,3},?(\d{3},?)*\d{3}(\.\d{0,2})?|\d{1,3}(\.\d{0,2})?|\.\d{1,2}?)$/,
     url:/^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
     required: function(){ 
       return (/(checkbox|radio)/.test(this[0].type) ? $("input[type='"+RegExp.$1+"'][name='"+this[0].name+"']").is(':checked') : ( this.val() != '' || this.val() != this.attr('title') )); 
     }, 
-    number: function() { return !isNaN(this.val());},
-    usd:/^\$?(\d{1,3},?(\d{3},?)*\d{3}(\.\d{0,2})?|\d{1,3}(\.\d{0,2})?|\.\d{1,2}?)$/,
+    numeric: function() { return !isNaN(this.val());},
     zip:/^\d{5}(-\d{4})?$/,
+    "length\\[(\\d+),?(\\d+)?\\]": function(min,max) {
+      var len = this.val().length; return len > min && len < max;
+    },
     phone:/^[2-9]\d{2}-\d{3}-\d{4}$/,
-    time12:/^[01]?\d:[0-5]\d?\s?[aApP]\.?[mM]\.?$/,
-    time24:/^(20|21|22|23|[01]\d|\d)(([:][0-5]\d){1,2})$/
+    time24:/^(20|21|22|23|[01]\d|\d)(([:][0-5]\d){1,2})$/,
+    time12:/^[01]?\d:[0-5]\d?\s?[aApP]\.?[mM]\.?$/
   };
 
-  /* Replacement patterns anything inside the #{...} will be replaced with the
-   * corresponding element's attribute That is to say "#{name}" will be
-   * replaced with the name attribute from the input, and "#{id}" with the id
-  */
   $.fn.okValidate.messages =  {
-    email:    "#{name} is not a valid address",
-    url:      "#{name} is not a valid url",
-    required: "#{name} is required",
-    number:   "#{name} is not a number",
-    usd:      "#{name} must be a US Dollar amount.",
-    zip:      "#{name} must be a zipcode in the format xxxxx or xxxxx-xxxx.",
-    phone:    "#{name} must be in the format xxx-xxx-xxxx.",
-    time24:   "#{name} must be a 24 hour time: 23:00.",
-    time12:   "#{name} must be a 12 hour time: 12:00 AM/PM"
+    email:    "#{field} is not a valid address",
+    url:      "#{field} is not a valid url",
+    required: "#{field} is required",
+    numeric:   "#{field} is not a number",
+    usd:      "#{field} must be a US Dollar amount.",
+    zip:      "#{field} must be a zipcode in the format xxxxx or xxxxx-xxxx.",
+    "length\\[(\\d+),?(\\d+)?\\]": function(name,min,max){
+      return name + (min && max ? " must be between "+min+" and "+max+" characters" : " must at least "+min+" characters");
+    },
+    phone:    "#{field} must be in the format xxx-xxx-xxxx.",
+    time24:   "#{field} must be a 24 hour time: 23:00.",
+    time12:   "#{field} must be a 12 hour time: 12:00 AM/PM"
   };
 })(jQuery);
