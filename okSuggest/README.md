617566b4faf27655256592862b341a0cc6f6969d
Asher Van Brunt <asher.vanbrunt@gmail.com>
2011-10-21 15:16:48 -0700

    [okSuggest] Allow loss of focus to hide results

--- okSuggest/README.md
+++ okSuggest/README.md
@@ -39,6 +39,7 @@ option           | default                                | description
 data             | []                                     | Data used in the liveSearch
 selected         | null                                   | String - which datum should start out in the input
 minLength        | 1                                      | Minimum number of keystrokes to start the search
+clearOnFocus     | true                                   | If focusing the input should clear the selected value
 notFound         | Function                               | Called in the event that the user-entered string does not conform to existing values (`this` is set to the original input)
 filter           | Function (see plugin)                  | Run for each data point, must return a numerical value (higher == better match)
 onInsert         | function(textInput,str)                | Called when a new value is inserted into the original input
--- okSuggest/jquery.okSuggest.js
+++ okSuggest/jquery.okSuggest.js
@@ -4,12 +4,12 @@
  * Copyright (c) 2011 Asher Van Brunt | http://www.okbreathe.com
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
- * Date: 03/11/11 
+ * Date: 09/18/11
  *
  * @projectDescription Live Search
  * @author Asher Van Brunt
  * @mailto asher@okbreathe.com
- * @version 1.0.0
+ * @version 1.1.0
  *
  */
 
@@ -19,18 +19,20 @@
   $.fn.okSuggest = function(opts){
 
     opts = $.extend({
-      data       : [],   // Data used in the liveSearch
-      selected   : null, // String - which datum should start out in the input
-      minLength  : 1,    // Minimum number of keystrokes to start the search
-      onInsert   : function(textInput,str) { // Called when a new value is inserted into the original input
+      data       : [],                           // Data used in the liveSearch
+      selected   : null,                         // String - which datum should start out in the input
+      minLength  : 1,                            // Minimum number of keystrokes to start the search
+      clearOnFocus: true,                        // If focusing the input should clear the selected value
+      hideListOnBlur: true,                      // Whether clicking outside the field will close the selection list
+      onInsert   : function(textInput,str) {     // Called when a new value is inserted into the original input
         return textInput.val(str);
-      },                                 // (`this` is set to the original input. Receives the visible text input as an argument)
-      notFound   : function(textInput){  // Called in the event that the user-entered string does not conform to existing values.
-      },                                 // (`this` is set to the original input. Receives the visible text input as an argument)
+      },                                         // (`this` is set to the original input. Receives the visible text input as an argument)
+      notFound   : function(textInput){},        // Called in the event that the user-entered string does not conform to existing values.
+                                                 // (`this` is set to the original input. Receives the visible text input as an argument)
       filter     : function(string,searchTerm) { // Run for each data point, must return a numerical value (higher == better match)
         return string.score(searchTerm);         // Uses quicksilver by default (included)
       },
-      helpText : "Begin typing to search" // Text displayed to user when focusing the input
+      helpText : "Begin typing to search"        // Text displayed to user when focusing the input
     },opts);
 
     function selectResult(liveSearch,forward){
@@ -90,21 +92,39 @@
     }
 
     function clickHandler(e){
-      var t         = $(e.target),
+      var t = $(e.target),
           okSuggest = t.closest('div.okSuggest');
       $("li.ui-selected", okSuggest).removeClass("ui-selected");
       t.addClass('ui-selected');
       insertResult(okSuggest.find(".okSuggest-input"),okSuggest.data("okSuggest"));
+      return false;
     }
 
     function toggleHelp(e) {
-      var vis = e.type == 'focusin';
-      $(e.target).nextAll(".okSuggest-help")[vis ? 'show' : 'hide']();
+      var t = $(e.target), help = t.nextAll(".okSuggest-help");
+      if (e.type == 'focusin') {
+        if (opts.clearOnFocus) { t.data('okSuggest-val', t.val()); t.val(''); }
+        help.show();
+      } else if (help.is(":visible")) {
+        help.hide();
+        if (e.type == 'focusout' && t.val() === '') {
+          t.val(t.data('okSuggest-val'));
+        }
+      }
     }
 
     if (!eventsBound) {
       $("input.okSuggest-input").live({ keydown: keydownHandler, keyup: keyupHandler, focus: toggleHelp, blur: toggleHelp });
-      $(".okSuggest .liveSearch li").live('click',clickHandler);
+      $("body").click(function(e){
+        var t = $(e.target), liveSearch = $('.okSuggest-list');
+        if (t.is(".okSuggest .liveSearch li")) {
+          clickHandler(e);
+        } else if (opts.hideListOnBlur) {
+          if (t.parents().index($("div.okSuggest")) <= 0 && liveSearch.is(":visible")) {
+            liveSearch.hide();
+          }
+        }
+      });
       eventsBound = true;
     }
 
@@ -115,17 +135,16 @@
         for ( var i = 0; i < this.children.length; i++ ) {
           opts.data.push($.trim( this.children[i].innerHTML )); 
         }  
-        val  = $("option:selected", self).html();
-        self = $("<input type='text' />").insertAfter(self.hide());
+        val  = opts.selected ? opts.selected : $("option:selected", self).html();
+        self = $("<input type='text' autocomplete='off' value='"+val+"' />").insertAfter(self.hide());
       }
-      if (opts.selected) { val = opts.selected; }
+
       liveSearch = $.liveSearch.create(opts.data, { className: "okSuggest-list" });
-      list       = liveSearch.contents().width(self.outerWidth(true));
+      list = liveSearch.contents().width(self.outerWidth(true));
 
       self
-        .attr({ autocomplete:'off', accesskey: self.attr("accesskey"), tabindex: self.attr("tabindex") })
+        .attr({ accesskey: self.attr("accesskey"), tabindex: self.attr("tabindex") })
         .addClass('okSuggest-input')
-        .val(val)
         .after(list.hide())
         .after("<div class='okSuggest-help' style='display: none;'>"+opts.helpText+"</div></div>")
         .parent()
