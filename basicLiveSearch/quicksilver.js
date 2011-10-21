e7ab5ee9e43be07858ad179b7d33b47ebb76eb1e
Asher Van Brunt <asher.vanbrunt@gmail.com>
2011-10-21 15:11:54 -0700

    Ensure quicksilver.js validates

--- basicLiveSearch/quicksilver.js
+++ basicLiveSearch/quicksilver.js
@@ -1,20 +1,8 @@
 // qs_score - Quicksilver Score
-// 
-// A port of the Quicksilver string ranking algorithm
-// 
-// "hello world".score("axl") //=> 0.0
-// "hello world".score("ow") //=> 0.6
-// "hello world".score("hello world") //=> 1.0
-//
-// Tested in Firefox 2 and Safari 3
-//
-// The Quicksilver code is available here
-// http://code.google.com/p/blacktree-alchemy/
-// http://blacktree-alchemy.googlecode.com/svn/trunk/Crucible/Code/NSString+BLTRRanking.m
 //
 // The MIT License
 // 
-// Copyright (c) 2008 Lachie Cox
+// Copyright (c) 2008 Lachie Cox, Modified by Asher Van Brunt
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -34,62 +22,57 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+var Quicksilver = {
+  score: function(string, abrev, offset) {
+    var sub_abrev, index, next_string, next_abrev, remaining_score,score,c;
+    offset = (offset || 0);
+   
+    if(abrev.length === 0)  {
+      return 0.9;
+    } else if (abrev.length > string.length)  {
+      return 0.0;
+    }
 
-String.prototype.score = function(abbreviation,offset) {
-  offset = offset || 0 // TODO: I think this is unused... remove
- 
-  if(abbreviation.length == 0) return 0.9
-  if(abbreviation.length > this.length) return 0.0
+    for (var i = abrev.length; i > 0; i--) {
+      sub_abrev = abrev.substring(0,i);
+      index     = string.indexOf(sub_abrev);
 
-  for (var i = abbreviation.length; i > 0; i--) {
-    var sub_abbreviation = abbreviation.substring(0,i)
-    var index = this.indexOf(sub_abbreviation)
+      if(index < 0 || (index + abrev.length > string.length + offset)) { continue; }
 
+      next_string       = string.substring(index+sub_abrev.length);
+      next_abrev = null;
 
-    if(index < 0) continue;
-    if(index + abbreviation.length > this.length + offset) continue;
-
-    var next_string       = this.substring(index+sub_abbreviation.length)
-    var next_abbreviation = null
-
-    if(i >= abbreviation.length)
-      next_abbreviation = ''
-    else
-      next_abbreviation = abbreviation.substring(i)
- 
-    var remaining_score   = next_string.score(next_abbreviation,offset+index)
- 
-    if (remaining_score > 0) {
-      var score = this.length-next_string.length;
-
-      if(index != 0) {
-        var j = 0;
-
-        var c = this.charCodeAt(index-1)
-        if(c==32 || c == 9) {
-          for(var j=(index-2); j >= 0; j--) {
-            c = this.charCodeAt(j)
-            score -= ((c == 32 || c == 9) ? 1 : 0.15)
-          }
-
-          // XXX maybe not port this heuristic
-          // 
-          //          } else if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:matchedRange.location]]) {
-          //            for (j = matchedRange.location-1; j >= (int) searchRange.location; j--) {
-          //              if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:j]])
-          //                score--;
-          //              else
-          //                score -= 0.15;
-          //            }
-        } else {
-          score -= index
-        }
+      if(i >= abrev.length) {
+        next_abrev = '';
+      } else {
+        next_abrev = abrev.substring(i);
       }
    
-      score += remaining_score * next_string.length
-      score /= this.length;
-      return score
+      remaining_score = Quicksilver.score(next_string,next_abrev,offset+index);
+   
+      if (remaining_score > 0) {
+        score = string.length-next_string.length;
+
+        if (index !== 0) {
+          c = string.charCodeAt(index-1);
+          if (c == 32 || c == 9) {
+            for (var j=(index-2); j >= 0; j--) {
+              c = string.charCodeAt(j);
+              score -= ((c == 32 || c == 9) ? 1 : 0.15);
+            }
+          } else {
+            score -= index;
+          }
+        }
+        score += remaining_score * next_string.length;
+        score /= string.length;
+        return score;
+      }
     }
+    return 0.0;
   }
-  return 0.0
-}
+};
+
+String.prototype.score = function(abbreviation,offset) {
+  return Quicksilver.score(this,abbreviation);
+};
